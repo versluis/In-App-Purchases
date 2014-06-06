fe2295d1bb748d375c39afdd535c4910c20e56a0
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2014-06-06 15:35:14 -0400

    Initial Commit

---
+++ InAppPurchases/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InAppPurchases
+//
+//  Created by Jay Versluis on 06/06/2014.
+//  Copyright (c) 2014 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InAppPurchases/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  InAppPurchases
+//
+//  Created by Jay Versluis on 06/06/2014.
+//  Copyright (c) 2014 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
